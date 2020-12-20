e8681f366689e8fa3c40b572bf37ad45d4208504
mohgeis <mohgeis@gmail.com>
2020-12-20 22:56:32 +0100

    add express

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const sensorEntity = require('./sensorEntity');
+const express = require('express');
 const mqtt = require('mqtt')
-const client = mqtt.connect('mqtt://broker.hivemq.com')
+
+const express_app = express();
+const mqtt_client = mqtt.connect('mqtt://broker.hivemq.com')
 
 var state = 'closed'
 
-client.on('connect', () => {
-    client.publish('parking/connected', 'true')
+mqtt_client.on('connect', () => {
+    mqtt_client.publish('parking/connected', 'true')
     console.log('connected')
 
     loopSendingUpdates();
@@ -14,7 +17,7 @@ client.on('connect', () => {
 
 function sendStateUpdate(sensorData) {
     console.log('sending state:', sensorData.name, ":", sensorData.attributes[0])
-    client.publish('parking/state', JSON.stringify(sensorData))
+    mqtt_client.publish('parking/state', JSON.stringify(sensorData))
 }
 
 async function loopSendingUpdates() {
@@ -28,3 +31,15 @@ async function loopSendingUpdates() {
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
+
+express_app.get('/test', (req, res) => {
+    res.send('An alligator approaches!');
+});
+
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+express_app.listen(port, () => console.log('Gator app listening on port 3000!'));
+
